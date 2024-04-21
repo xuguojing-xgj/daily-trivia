db411f201356ca79c91cf9cec86dbd65d341cc90
xuguojing-xgj <xuguojing77@163.com>
2024-04-22 02:38:55 +0800

    docs: 首页布局

--- src/components/HomepageFeatures/index.jsx
+++ src/components/HomepageFeatures/index.jsx
@@ -5,8 +5,10 @@ import styles from "./styles.module.css";
 import React, { } from "react";
 import VueImg from './images/Vue.png'
 import ReactImg from './images/React.png'
-
-const Card = ({id, img, name, describe}) => {
+import ESLintImg from "./images/eslint.png";
+import PrettierImg from './images/prettier.png'
+import NodeImg from './images/node.png'
+const Card = ({ id, img, name, describe }) => {
     const options = [
         {
             id,
@@ -32,12 +34,17 @@ const Card = ({id, img, name, describe}) => {
 
 export default function HomepageFeatures() {
     return (
-        <div className={styles.mianContainer}>
-            <Card id={1} img={VueImg} name={'Vue'} describe={'渐进式 JavaScript 框架'}></Card>
-            <Card id={2} img={ReactImg} name={'React'} describe={'渐进式 JavaScript 框架'}></Card>
-            <Card id={1} img={img1} name={'Vue'} describe={'渐进式 JavaScript 框架'}></Card>
-            <Card id={1} img={img1} name={'Vue'} describe={'渐进式 JavaScript 框架'}></Card>
-            <Card id={1} img={img1} name={'Vue'} describe={'渐进式 JavaScript 框架'}></Card>
+        <div >
+            <div className={styles.topContainer}>
+                <Card id={1} img={VueImg} name={'Vue'} describe={'渐进式 JavaScript 框架'}></Card>
+                <Card id={2} img={ReactImg} name={'React'} describe={'用于构建 Web 和原生交互界面的库'}></Card>
+                <Card id={3} img={ESLintImg} name={'ESLint'} describe={'查找并修复 JavaScript 代码中的问题'}></Card>
+                <Card id={4} img={PrettierImg} name={'Prettier'} describe={'一个“有态度”的代码格式化工具'}></Card>
+
+            </div>
+            <div className={styles.bottomContainer}>
+                <Card id={5} img={NodeImg} name={'Node'} describe={'JavaScript 运行时'}></Card>
+            </div>
         </div>
     );
 }
